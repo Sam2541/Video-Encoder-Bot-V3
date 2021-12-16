382b560315ae55e2d034378afdcfd182c8af1480
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-16 17:57:39 +0530

    Update ffmpeg.py

--- bot/helper_funcs/ffmpeg.py
+++ bot/helper_funcs/ffmpeg.py
@@ -102,7 +102,7 @@ async def convert_video(video_file, output_directory, total_time, bot, message, 
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 10))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(10 - math.floor(percentage / 10))])
             )
-        stats = f'📦️ <b>Compressing</b> {target_percentage}%\n\n' \
+        stats = f'📦️ <b>Encoding Is In Progress%\n\n' \
                 f'⏰️ <b>ETA:</b> {ETA}\n\n' \
                 f'{progress_str}\n'
         try:
